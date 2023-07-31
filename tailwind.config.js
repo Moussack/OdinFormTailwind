0dc096e94a56af8048c42bfa8a16f5d61f1310b4
moussack <reywahyono@gmail.com>
2023-07-31 15:08:22 +0700

    styling input

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: ['./build/*'],
+   content: ['./build/**/*.{html,js}'],
    theme: {
       extend: {
          backgroundImage: {
