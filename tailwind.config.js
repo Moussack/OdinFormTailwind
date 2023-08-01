7fb8c8f1c9534d6598f7e0100d0d13b456e09368
moussack <reywahyono@gmail.com>
2023-08-01 14:30:38 +0700

    change bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ module.exports = {
       extend: {
          backgroundImage: {
             'background-Img':
-               "url('https://images.unsplash.com/photo-1680402809428-507ad4e45ebd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1970&q=80')",
+               "url('https://images.unsplash.com/photo-1520176501380-9a174bf7c783?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80')",
          },
          colors: {
-            'custom-transparent': 'rgba(0, 0, 0, 0.5)',
+            'custom-transparent': 'rgba(0, 0, 0, 0.6)',
          },
       },
    },
